ff9f96166d2fea2159c6561f4659bec428ed7f74
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:16:03 +0200

    FEAT: add new info

--- src/main/java/it/itsacademy/gestione_ordini/GestioneOrdiniApplication.java
+++ src/main/java/it/itsacademy/gestione_ordini/GestioneOrdiniApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestioneOrdiniApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestioneOrdiniApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneOrdiniApplication.class, args);
+    }
 
 }
--- src/test/java/it/itsacademy/gestione_ordini/GestioneOrdiniApplicationTests.java
+++ src/test/java/it/itsacademy/gestione_ordini/GestioneOrdiniApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestioneOrdiniApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
