5836f6ee7270bc5e937abc04cb5606951493df5a
Haukeys <leopazienza76@gmail.com>
2026-05-25 17:38:41 +0200

    FEAT: New Mapper

---
+++ src/main/java/it/itsacademy/gestione_ordini/mapper/OrdineMapper.java
@@ -0,0 +1,22 @@
+package it.itsacademy.gestione_ordini.mapper;
+
+
+import it.itsacademy.gestione_ordini.dto.OrdineCreateDTO;
+import it.itsacademy.gestione_ordini.dto.OrdineDTO;
+import it.itsacademy.gestione_ordini.dto.OrdineResponseDTO;
+import it.itsacademy.gestione_ordini.entity.Ordine;
+import it.itsacademy.gestione_ordini.entity.TipoOrdine;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring",imports = {TipoOrdine.class})
+public interface OrdineMapper {
+
+    @Mapping(target = "idOrdine", ignore = true)
+    @Mapping(target="statoOrdine",ignore = true)
+    Ordine toOrdine(OrdineCreateDTO ordineDTO);
+
+    OrdineDTO toOrdineDTO(Ordine ordine);
+
+    OrdineResponseDTO toOrdineResponseDTO(Ordine ordineResponse);
+}
