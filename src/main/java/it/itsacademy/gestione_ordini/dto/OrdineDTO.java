5d27d41dcf37c25214b3097235969db3c31978f9
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:59:09 +0200

    FEAT add a validation

--- src/main/java/it/itsacademy/gestione_ordini/dto/OrdineDTO.java
+++ src/main/java/it/itsacademy/gestione_ordini/dto/OrdineDTO.java
@@ -3,6 +3,7 @@ package it.itsacademy.gestione_ordini.dto;
 import it.itsacademy.gestione_ordini.entity.TipoOrdine;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,17 +17,24 @@ import java.util.UUID;
 
 public class OrdineDTO {
 
+    @NotNull(message = "idOrdine non puo essere null")
+    @NotBlank(message = "idOrdine non puo essere vuoto")
     private UUID idOrdine;
+
     @NotBlank(message = "data di creazione non puo essere vuoto")
     @NotNull(message = "data di creazione non puo essere null")
     private LocalDate dataCreazione;
+
     @NotBlank(message = "descrizione non puo essere vuoto")
     @NotNull(message = "descrizione non puo essere vuoto")
     private String descrizione;
+
     @NotBlank(message = "statoOrdine non puo essere vuoto")
     @NotNull(message = "statoOrdine non puo essere vuoto")
     private TipoOrdine statoOrdine;
+
     @NotBlank(message = "totale non puo essere vuoto")
     @NotNull(message = "totale non puo essere vuoto")
+    @Positive(message = "totale deve essere positivo")
     private Double totale;
 }
