f1cb3986777432540a473629e6d9608cf435ba18
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:13:06 +0200

    Initial commit

---
+++ src/main/java/it/itsacademy/gestione_ordini/GestioneOrdiniApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestione_ordini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneOrdiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneOrdiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/itsacademy/gestione_ordini/GestioneOrdiniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestione_ordini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneOrdiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
