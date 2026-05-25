6e2d80288cd263e4dd57949eeba324e3a5a7d7ae
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:16:43 +0200

    FEAT: add new dto

---
+++ src/main/java/it/itsacademy/gestione_ordini/dto/OrdineDTO.java
@@ -0,0 +1,32 @@
+package it.itsacademy.gestione_ordini.dto;
+
+import it.itsacademy.gestione_ordini.entity.TipoOrdine;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class OrdineDTO {
+
+    private UUID idOrdine;
+    @NotBlank(message = "data di creazione non puo essere vuoto")
+    @NotNull(message = "data di creazione non puo essere null")
+    private LocalDate dataCreazione;
+    @NotBlank(message = "descrizione non puo essere vuoto")
+    @NotNull(message = "descrizione non puo essere vuoto")
+    private String descrizione;
+    @NotBlank(message = "statoOrdine non puo essere vuoto")
+    @NotNull(message = "statoOrdine non puo essere vuoto")
+    private TipoOrdine statoOrdine;
+    @NotBlank(message = "totale non puo essere vuoto")
+    @NotNull(message = "totale non puo essere vuoto")
+    private Double totale;
+}
