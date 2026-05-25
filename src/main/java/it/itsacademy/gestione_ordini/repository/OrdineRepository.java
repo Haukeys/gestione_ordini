4c0e491aac263ae1e795b3b04bd36bfd88d0e433
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:28:07 +0200

    FIX: integer into UUID

--- src/main/java/it/itsacademy/gestione_ordini/repository/OrdineRepository.java
+++ src/main/java/it/itsacademy/gestione_ordini/repository/OrdineRepository.java
@@ -3,6 +3,8 @@ package it.itsacademy.gestione_ordini.repository;
 import it.itsacademy.gestione_ordini.entity.Ordine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrdineRepository extends JpaRepository<Ordine, Integer> {
+import java.util.UUID;
+
+public interface OrdineRepository extends JpaRepository<Ordine, UUID> {
 
 }
