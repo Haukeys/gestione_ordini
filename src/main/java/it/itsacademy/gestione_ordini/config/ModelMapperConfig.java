899770958f9c9f03a98ce9311554e2026d494452
Haukeys <leopazienza76@gmail.com>
2026-07-02 10:35:44 +0000

    FEAT: ADD AUDIT CONFIG

---
+++ src/main/java/it/itsacademy/gestione_ordini/config/ModelMapperConfig.java
@@ -0,0 +1,24 @@
+package it.itsacademy.gestione_ordini.config;
+
+
+import it.itsacademy.gestione_ordini.service.AuditorAwareImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "getAuditorAwareImpl")
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    AuditorAware<String> getAuditorAwareImpl(){
+        return new AuditorAwareImpl();
+    }
+}
